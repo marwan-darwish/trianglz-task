6bc2b59c6017bc799afacee24147dd74540019b4
marwan darwish <darawish663@gmail.com>
2023-05-18 00:33:46 +0200

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  AcoreAPIKEY:"AIzaSyAfK6BssxRTiU3mNQEzL7jz0PcRZbs7sPg",
+  host:"books-7dee0.firebaseapp.com",
+  firebaseConfig : {
+    apiKey: "AIzaSyAfK6BssxRTiU3mNQEzL7jz0PcRZbs7sPg",
+    authDomain: "books-7dee0.firebaseapp.com",
+    projectId: "books-7dee0",
+    storageBucket: "books-7dee0.appspot.com",
+    messagingSenderId: "340890073086",
+    appId: "1:340890073086:web:41e1c4f7327ccdf28072af"
+  },
   production: true
 };
